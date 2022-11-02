bc918b1898a19131833a148ad1aa24f21a8977b7
Michael Gall <mgall@landlordwebsolutions.com>
2022-11-02 09:04:06 -0400

    Initial Commit

---
+++ Instacard/InstacardApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstacardApp.swift
+//  Instacard
+//
+//  Created by Michael Gall on 2022-11-02.
+//
+
+import SwiftUI
+
+@main
+struct InstacardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
